5d75033bd39ff0daee0e13fcc5841162a463811c
Sora Harakami <sora134@gmail.com>
2010-05-21 13:20:11 +0900

    Refactoring (23

--- Classes/udon_tairikuViewController.m
+++ Classes/udon_tairikuViewController.m
@@ -16,8 +16,6 @@
 
 @synthesize twit;
 
-
-// The designated initializer. Override to perform setup that is required before the view is loaded.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
         // Custom initialization
@@ -83,16 +81,10 @@
 }
 
 - (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
+- (void)viewDidUnload {}
 
 - (IBAction)clearButtonIsPushed: (id)sender {
 	if ([tv.text isEqualToString:@""]) {
